a8c23f6f5a7bcb616c9f36e20e0ee6ca609d9642
Ra1dBOSS <boss97cool@mail.ru>
2018-07-04 09:56:51 +0300

    initial commit

---
+++ src/main/java/com/solutions/travel/on/voting_project/VotingProjectApplication.java
@@ -0,0 +1,12 @@
+package com.solutions.travel.on.voting_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingProjectApplication.class, args);
+	}
+}
